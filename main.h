02f4c0f1e16c9fe27341b5e948d839968241e16b
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-08-21 10:19:07 +0000

    View mainh

--- main.h
+++ main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-#include <errno.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdeff.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <dirent.h>
+char *trim(char *string);
+
 #endif
