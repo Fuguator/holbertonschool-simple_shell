024384822812f394474c17cfb4337a588435cd3b
Your Name <you@example.com>
2024-08-22 11:28:01 +0400

    main.h changed

--- main.h
+++ main.h
@@ -1,15 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <errno.h>
-#include <dirent.h>
-char *trim(char *string);
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 extern char **environ;
+
+char **divider(char *buffer, char **arr);
+char *_getline(void);
+void _exec(char *buffer, char **argv);
+void find_path(char *buffer, char **argv);
+int handle_path(char *buffer);
 void print_env(char *buffer);
 #endif
